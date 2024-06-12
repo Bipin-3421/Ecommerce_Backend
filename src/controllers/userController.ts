e1bd0d39fee368e975a59f151b33a84e00d78db5
Bipin Baral <baburaja3421@gmail.com>
2024-06-13 02:25:53 +0545

    feat: User Authentication

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,36 +1,54 @@
 import express, { Request, Response, NextFunction } from "express";
 import bcrypt from "bcryptjs";
 import { User } from "../models/user.js";
-import { NewUserRequestBody } from "../types/types.js";
 import jwt from "jsonwebtoken";
+import ErrorHandler from "../utils/error-utility-class.js";
+import { sendCookie } from "../utils/features.js";
 
 export const register = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const { name, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
-  const token = jwt.sign({ _id: user._id }, process.env.jwt as string);
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const token = jwt.sign({ _id: user._id }, process.env.jwt as string);
 
-  res.cookie("token", token, {
-    httpOnly: true,
-    maxAge: 15 * 60 * 1000,
-  });
-  res.status(201).json({
-    success: true,
-    message: "Registered Successfully",
-    token,
-  });
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 15 * 60 * 1000,
+    });
+    res.status(201).json({
+      success: true,
+      message: "Registered Successfully",
+      token,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
 
-  res.status(200).json({
-    success: true,
-    message: "User Successfully Registered",
-    user,
-  });
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) return next(new ErrorHandler("user not found", 404));
+    const isUserMatch = await bcrypt.compare(password, user.password);
+    if (!isUserMatch)
+      return next(new ErrorHandler("Incorrect Credentials", 404));
+    const token = jwt.sign({ _id: user._id }, process.env.jwt as string);
+    sendCookie(user, res, `Welcome back, ${user.name}`, 200);
+  } catch (err) {
+    next(err);
+  }
 };
--- src/middlewares/isAdmin.ts
+++ src/middlewares/isAdmin.ts
@@ -1,10 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/user.js";
+import ErrorHandler from "../utils/error-utility-class.js";
 
 const isAdmin = (req: Request, res: Response, next: NextFunction) => {
-    if(!req.user) {
-        
-    }
+  if (!req.user) {
+    return next(new ErrorHandler("Unauthorized", 401));
+  }
+  if (req.user.role !== "admin") {
+    return next(new ErrorHandler("Forbidden: Admins only", 401));
+  }
+
+  next();
 };
 
 export default isAdmin;
